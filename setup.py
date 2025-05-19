b92bfeef1c3c66e1343b069d495500a4241b7852
Taha Zahid <tz09220@st.habib.edu.pk>
2025-05-20 01:50:36 +0500

    first commit

---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import find_packages, setup
+
+package_name = 'my_robot_rviz'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/meshes', [
+        'meshes/Base.dae',
+        'meshes/Antebrazo.dae',
+        'meshes/Basedelagarra.dae',
+        'meshes/Brazo.dae',
+        'meshes/EjeCentral.dae',
+        'meshes/Muteca.dae',
+        'meshes/Pinza1.dae',
+        'meshes/Pinza2.dae',
+    ]),
+        ('share/' + package_name + '/urdf', ['urdf/my_robot.urdf']),
+        ('share/' + package_name + '/launch', ['launch/display.launch.py']),
+        
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='taha',
+    maintainer_email='taha@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
